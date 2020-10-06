5417dced550225de071484627a0e3fc9168b8eb2
Ebenezer Justice <justiceebenezer4@gmail.com>
2020-10-06 09:05:52 +0100

    initial commits

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -35,4 +35,13 @@ public class InfoActivity extends AppCompatActivity {
        }
 
     }
+    public void createPhoneIntent(View view){
+        Uri uri = Uri.parse("tel:0123456789");
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(uri);
+        startActivity(intent);
+
+
+
+    }
 }
